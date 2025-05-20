5ee1210b18fb35a47c7ef334ccc298a6281fbf80
JonthanAyala <20223tn085@utez.edu.mx>
2025-05-19 18:23:08 -0600

    Inicio

---
+++ servidor/src/main/java/mx/edu/utez/servidor/ServidorApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorApplication.class, args);
+	}
+
+}
